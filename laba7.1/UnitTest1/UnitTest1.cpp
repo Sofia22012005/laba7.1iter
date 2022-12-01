a52203a3a7954695056b2a202dd7b9bb098011be
Sofia22012005 <114824939+Sofia22012005@users.noreply.github.com>
2022-12-01 02:30:05 +0200

    Add files via upload

---
+++ laba7.1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba7.1/laba7.1.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(testLab)
+		{
+
+			// int rowCount = 8;
+			 // int colCount = 5;
+			const int n = 10;
+			int *a[n];
+			int S = 0;
+			int k = 0;
+			 res = Calc(a, 8, 5,S,k);
+			Assert::AreEqual(res, 5);
+
+		}
+	};
+}
